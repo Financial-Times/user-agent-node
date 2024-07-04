60f8ad66e7d0d659a666d74d304c42396b0c64d2
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2024-07-05 00:18:31 +0100

    chore: migrate to the Tool Kit beta
    The beta is way faster and Platforms need more people to test it.

--- .toolkit/plugins/node-test/index.js
+++ .toolkit/plugins/node-test/index.js
@@ -1,13 +1,11 @@
 const { fork } = require('node:child_process');
 const { hookFork, waitOnExit } = require('@dotcom-tool-kit/logger');
-const { Task } = require('@dotcom-tool-kit/types');
+const { Task } = require('@dotcom-tool-kit/base');
 
-class NodeTest extends Task {
+module.exports = class NodeTest extends Task {
 	async run() {
 		const child = fork('--test', ['--test-reporter', 'spec'], { silent: true });
 		hookFork(this.logger, 'node test', child);
 		return waitOnExit('node test', child);
 	}
-}
-
-exports.tasks = [NodeTest];
+};
