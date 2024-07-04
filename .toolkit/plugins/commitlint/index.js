1e5cd2c701fca643b92bc1dcd33a58d65b15c96d
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2024-07-05 00:18:31 +0100

    chore: migrate to the Tool Kit beta
    The beta is way faster and Platforms need more people to test it.

--- .toolkit/plugins/commitlint/index.js
+++ .toolkit/plugins/commitlint/index.js
@@ -1,15 +1,13 @@
 const { fork } = require('node:child_process');
 const { hookFork, waitOnExit } = require('@dotcom-tool-kit/logger');
-const { Task } = require('@dotcom-tool-kit/types');
+const { Task } = require('@dotcom-tool-kit/base');
 
 const commitlintCLIPath = require.resolve('.bin/commitlint');
 
-class Commitlint extends Task {
+module.exports = class Commitlint extends Task {
 	async run() {
 		const child = fork(commitlintCLIPath, ['--edit'], { silent: true });
 		hookFork(this.logger, 'commitlint', child);
 		return waitOnExit('commitlint', child);
 	}
-}
-
-exports.tasks = [Commitlint];
+};
